30e28aa5df3a7f1abcb2900bd182a57cb657c975
anneliev <annelieviklund@gmail.com>
2017-03-27 17:42:04 +0200

    added README, improved error handling, added loading symbol

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,53 +1,59 @@
-
-
+//storing HTML elements using Object literal to be albe to call them and to have them in the same namespace
 const elem = {
 	wetherToShow: document.getElementById("weatherToShow"), 
 	currentBtn: document.getElementById("currentBtn"),
 	forecastBtn: document.getElementById("forecastBtn"),
 	sunriseBtn: document.getElementById("sunriseBtn"),
-	displayArea: document.getElementById("displayArea")
+	displayArea: document.getElementById("displayArea"),
+	loadsymbol: document.getElementById("loadsymbol")
 };
 
+//Module pattern using IIFE with event listeners on the buttons on HTML page. Calling functions when clicked on
 const searchButtons = (function(){
 	elem.currentBtn.addEventListener("click", function(){
-		dataBase.getCurrent();
+		weatherFunctions.getCurrent();//calling the function contianing the data and AJAX request
 	});
 
 	elem.forecastBtn.addEventListener("click", function(){
-		dataBase.getForecast();
+		weatherFunctions.getForecast();
 	});
 
 	elem.sunriseBtn.addEventListener("click", function(){
-		dataBase.getSunrise();
+		weatherFunctions.getSunrise();
 	});
 
 });
-searchButtons();
+searchButtons();//calling the function holdning the buttons
 
-const dataBase = {
+const weatherFunctions = {//Object literl holdning the functions that are being called by searchButtons
 	getCurrent: () => {
-		let searchValue = searchCurrent.value;
-		let apiKey = "&APPID=546212d0f83942c04cc3caec6ee321c9";
-		let api = "http://api.openweathermap.org/data/2.5/weather?q=";
-		let info = api + searchValue + apiKey;
-			
-		$.get(info)
-			.done((response) => {
-				console.log("funkar");
+		let searchValue = searchCurrent.value;//user input value in search box
+		let apiKey = "&APPID=546212d0f83942c04cc3caec6ee321c9";//variable holding the APIkey
+		let api = "http://api.openweathermap.org/data/2.5/weather?q=";//the url to the API
+		let info = api + searchValue + apiKey;//variable with the vaiables combined, used later as an argument
+		utilityFunctions.show();//calling the function with the loading symbol
+
+		$.get(info)//AJAX GET request with an arugument
+			.done((response) => {//if data is successfully loaded
+				console.log("Successfully loaded data");
 				let theWeather = response;
 				console.log(theWeather);
+				//template literal used to display the AJAX request on the HTML page
 				let weatherToShow = `<li class="weatherDisplay">
-				<h1>${theWeather.name}</h1><br />
-				<img src="http://openweathermap.org/img/w/${theWeather.weather[0].icon}.png"></img><br /><br />
+				<h1>${theWeather.name}</h1>
+				<h4>${utilityFunctions.dateConverter(theWeather.dt)}</h4><br />
+				<img src="https://openweathermap.org/img/w/${theWeather.weather[0].icon}.png"></img><br /><br />
 				<p>Weather: <strong>${theWeather.weather[0].main}</strong>, ${theWeather.weather[0].description}</p>
 				<p>Temperature: ${parseFloat(theWeather.main.temp - 273.15).toFixed(1)} °C</p>
 				</li>`;
-				displayArea.innerHTML = weatherToShow;
+				utilityFunctions.hide();//calling the function that hides the loading symbol, after the request is done
+				displayArea.innerHTML = weatherToShow;//dispalying the data on HTML page
 			})
-			.fail((error) => {
-				console.log("funkar inte");
+			.fail((error) => {//if data isn't successfully loaded, an alert with error message
+				alert ("Error. The requested data couldn't load. Type the name of a city in the search box or try to temporarily allow unsecure script on this page.");
+				utilityFunctions.hide();//calling the function that hides the loading symbol, after the request is done
 			});
-		searchCurrent.value = "";
+		searchCurrent.value = "";//clearing the search box
 	},
 
 	getForecast: () => {
@@ -56,31 +62,33 @@ const dataBase = {
 		let api = "http://api.openweathermap.org/data/2.5/forecast/daily?q=";
 		let days = "&cnt=6";
 		let info = api + searchValue + days + apiKey;
+		utilityFunctions.show();
 	
 		$.get(info)
 			.done((response) => {
-				console.log("funkar");
+				console.log("Successfully loaded data");
 				let theWeather = response;
 				console.log(theWeather);
-
-				displayArea.innerHTML = "";
 			
 				let city = `<li class="weatherDisplay">
 				<h1>${theWeather.city.name}</h1><br />
 				</li>`;
 				displayArea.innerHTML = city;
-				for(let i = 0; i < 6; i++){
+				for(let i = 0; i < 6; i++){//a loop that goes thru 5 index in the array that is requested.
+					//a function is called to convert the date in the object, from unix numbers
 					let wetherToShow = `<li class="weatherDisplay">
-					<h4>${dataBase.dateConverter(theWeather.list[i + 1].dt)}</h4>
-					<img src="http://openweathermap.org/img/w/${theWeather.list[i].weather[0].icon}.png"></img><br /><br />
+					<h4>${utilityFunctions.dateConverter(theWeather.list[i + 1].dt)}</h4>
+					<img src="https://openweathermap.org/img/w/${theWeather.list[i].weather[0].icon}.png"></img><br /><br />
 					<p>Weather: <strong>${theWeather.list[i].weather[0].main}</strong>, ${theWeather.list[i].weather[0].description}</p>
 					<p>Temperature: ${parseFloat(theWeather.list[i].temp.day - 273.15).toFixed(1)} °C</p>
 					</li><br /><br />`;
+					utilityFunctions.hide();
 					displayArea.innerHTML += wetherToShow;
 				}
 			})
 			.fail((error) => {
-				console.log("funkar inte");
+				alert ("The requested data couldn't load. Type the name of a city in the search box or try to temporarily allow unsecure script on this page.");
+				utilityFunctions.hide();
 			});
 		searchForecast.value = "";
 	},
@@ -90,29 +98,34 @@ const dataBase = {
 		let apiKey = "&APPID=546212d0f83942c04cc3caec6ee321c9";
 		let api = "http://api.openweathermap.org/data/2.5/weather?q=";
 		let info = api + searchValue + apiKey;
+		utilityFunctions.show();
 
 		$.get(info)
 			.done((response) => {
-				console.log("funkar");
+				console.log("Successfully loaded data");
 				let theWeather = response;
 				console.log(theWeather);
 				let weatherToShow = `<li class="weatherDisplay">
 				<h1>${theWeather.name}</h1>
-				<h4>${dataBase.dateConverter(theWeather.sys.sunrise)}</h4><br />
-				<img src="http://openweathermap.org/img/w/01d.png"></img>
-				<p><strong>Sunrise: </strong>${dataBase.timeConverter(theWeather.sys.sunrise)}</p><br />
-				<img src="http://openweathermap.org/img/w/01n.png"></img>
-				<p><strong>Sunset: </strong>${dataBase.timeConverter(theWeather.sys.sunset)}</p>
+				<h4>${utilityFunctions.dateConverter(theWeather.sys.sunrise)}</h4><br />
+				<img src="https://openweathermap.org/img/w/01d.png"></img>
+				<p><strong>Sunrise: </strong>${utilityFunctions.timeConverter(theWeather.sys.sunrise)}</p><br />
+				<img src="https://openweathermap.org/img/w/01n.png"></img>
+				<p><strong>Sunset: </strong>${utilityFunctions.timeConverter(theWeather.sys.sunset)}</p>
 				</li>`;
+				utilityFunctions.hide();
 				displayArea.innerHTML = weatherToShow;
 			})
 			.fail((error) => {
-				console.log("funkar inte");
+				alert ("The requested data couldn't load. Type the name of a city in the search box or try to temporarily allow unsecure script on this page.");
+				utilityFunctions.hide();
 			});
 		searchSunrise.value = "";
-	},
+	}
+};
 
-	timeConverter: (unixTime) => {
+const utilityFunctions = {//object literal holdning utility functions
+	timeConverter: (unixTime) => {//converts time from unix format
 		let time = new Date(unixTime * 1000);
 		let hour = time.getHours();
 		let min = time.getMinutes();
@@ -120,7 +133,7 @@ const dataBase = {
 		return convertedTime;
 	},
 
-	dateConverter: (unixTime) => {
+	dateConverter: (unixTime) => {//converts date from unix format
 		let time = new Date(unixTime * 1000);
 		let date = time.getDate();
 		let month = time.getMonth() + 1;
@@ -128,39 +141,14 @@ const dataBase = {
 		return convertedTime;
 	},
 
-
-};
-
-
-
-
-//fixa loadsymbol
-//fixa success och error hantering
-
-
-/*
-$.get(info, (response) => {
-			let theWeather = response;
-			console.log(theWeather);
-			displayArea.innerHTML = "";
-			
-			let city = `<li class="weatherDisplay">
-			<h1>${theWeather.city.name}</h1><br />
-			</li>`;
-			displayArea.innerHTML = city;
-			for(let i = 0; i < 6; i++){
-				let wetherToShow = `<li class="weatherDisplay">
-				<h4>${dataBase.dateConverter(theWeather.list[i + 1].dt)}</h4>
-				<img src="http://openweathermap.org/img/w/${theWeather.list[i].weather[0].icon}.png"></img><br /><br />
-				<p>Weather: <strong>${theWeather.list[i].weather[0].main}</strong>, ${theWeather.list[i].weather[0].description}</p>
-				<p>Temperature: ${parseFloat(theWeather.list[i].temp.day - 273.15).toFixed(1)} °C</p>
-				</li><br /><br />`;
-				displayArea.innerHTML += wetherToShow;
-			}
-		
-
-		});
-		searchForecast.value = "";
+	show: () => {//shows the loading symbol
+		elem.loadsymbol.style.display = "block";
+		//alert("loading");
 	},
 
-	*/
+	hide: () => {//hides the loading symbol
+		//alert("done loading");
+		elem.loadsymbol.style.display = "none";
+	}
+};
+
